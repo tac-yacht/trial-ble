547f6102f3cea8444a2f8e2430544fbe64f3e987
tac-yacht <tac-yacht@users.noreply.github.com>
2024-07-20 14:22:54 +0900

    Create wifi.py

---
+++ wifi.py
@@ -0,0 +1,16 @@
+import network
+from time import sleep
+
+ssid = '【置換】'
+password = '【置換】'
+
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+    ip = wlan.ifconfig()[0]
+    print(f'Connected on {ip}')
