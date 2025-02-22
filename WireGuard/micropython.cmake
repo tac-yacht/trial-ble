4c5445c4ec0ef2b540b12b2226f4b2908f3f45eb
tac-yacht <tac-yacht@users.noreply.github.com>
2025-02-22 20:23:17 +0900

    スタンドアロンな暗号ライブラリ部分読み込みテスト

---
+++ WireGuard/micropython.cmake
@@ -0,0 +1,15 @@
+# Create an INTERFACE library for our CPP module.
+add_library(usermod_wireguard INTERFACE)
+
+# Add our source files to the library.
+target_sources(usermod_wireguard INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/src/crypto/refc/x25519.c
+)
+
+# Add the current directory as an include directory.
+target_include_directories(usermod_wireguard INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}
+)
+
+# Link our INTERFACE library to the usermod target.
+target_link_libraries(wireguard INTERFACE usermod_wireguard)
