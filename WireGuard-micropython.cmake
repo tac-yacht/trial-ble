52be8b3a742cf319e5c2edbc1ca591a55c22b8b8
tac-yacht <tac-yacht@users.noreply.github.com>
2025-03-05 17:28:46 +0900

    WireGuard.cpp有効化 既出のログ削除も同時に実施

--- WireGuard-micropython.cmake
+++ WireGuard-micropython.cmake
@@ -10,7 +10,7 @@ target_sources(usermod_wireguard INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/crypto/refc/x25519.c
     ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/crypto.c
     ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/wireguard.c
-#    ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/WireGuard.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/WireGuard.cpp
     ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/wireguardif.c
     ${CMAKE_CURRENT_LIST_DIR}/WireGuard/src/wireguard-platform.c
 )
