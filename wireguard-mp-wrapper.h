4234e36f14d7ae17bda43cb84b2fa692a6fd9b38
tac-yacht <tac-yacht@users.noreply.github.com>
2025-04-29 19:19:04 +0900

    使用するマクロ変更

--- wireguard-mp-wrapper.h
+++ wireguard-mp-wrapper.h
@@ -14,8 +14,8 @@ static const MP_DEFINE_STR_OBJ(subnet_default, "255.255.255.255");
 static const MP_DEFINE_STR_OBJ(gateway_default, "0.0.0.0");
 const mp_arg_t begin_allowed_args[] = {
 	{MP_QSTR_local_ip, MP_ARG_OBJ|MP_ARG_REQUIRED},
-	{MP_QSTR_subnet, MP_ARG_OBJ, {.u_rom_obj = MP_ROM_QSTR(&subnet_default)}},
-	{MP_QSTR_gateway, MP_ARG_OBJ, {.u_rom_obj = MP_ROM_QSTR(&gateway_default)}},
+	{MP_QSTR_subnet, MP_ARG_OBJ, {.u_rom_obj = MP_ROM_PTR(&subnet_default)}},
+	{MP_QSTR_gateway, MP_ARG_OBJ, {.u_rom_obj = MP_ROM_PTR(&gateway_default)}},
 	{MP_QSTR_private_key, MP_ARG_OBJ|MP_ARG_REQUIRED},
 	{MP_QSTR_remote_peer_address, MP_ARG_OBJ|MP_ARG_REQUIRED},
 	{MP_QSTR_remote_peer_public_key, MP_ARG_OBJ|MP_ARG_REQUIRED},
