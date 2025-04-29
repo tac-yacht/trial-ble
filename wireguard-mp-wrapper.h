2ec3e8ede88b814ed0ee7b1380fcaf3c364429e6
tac-yacht <tac-yacht@users.noreply.github.com>
2025-04-29 13:42:56 +0900

    コメント位置修正

--- wireguard-mp-wrapper.h
+++ wireguard-mp-wrapper.h
@@ -2,5 +2,9 @@
 #include "py/obj.h"
 
 mp_obj_t begin(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args)
-/*None*/mp_obj_t end();
-/*bool*/mp_obj_t is_initialized();
+
+/*None*/
+mp_obj_t end();
+
+/*bool*/
+mp_obj_t is_initialized();
