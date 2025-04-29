55a05533eacf21303ce8a964ef410bc1c9cdda7b
tac-yacht <tac-yacht@users.noreply.github.com>
2025-04-29 14:21:49 +0900

    C言語であることを明示

--- wireguard-mp-wrapper.h
+++ wireguard-mp-wrapper.h
@@ -1,4 +1,8 @@
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "py/obj.h"
 
 mp_obj_t begin(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
@@ -8,3 +12,7 @@ mp_obj_t end();
 
 /*bool*/
 mp_obj_t is_initialized();
+
+#ifdef __cplusplus
+}
+#endif
