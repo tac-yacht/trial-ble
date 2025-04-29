986fdb0b35ff19bdad159e7f0c376a27ab601980
tac-yacht <tac-yacht@users.noreply.github.com>
2025-04-29 14:03:32 +0900

    末尾のセミコロン忘れてた

--- wireguard-mp-wrapper.h
+++ wireguard-mp-wrapper.h
@@ -1,7 +1,7 @@
 
 #include "py/obj.h"
 
-mp_obj_t begin(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args)
+mp_obj_t begin(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
 
 /*None*/
 mp_obj_t end();
